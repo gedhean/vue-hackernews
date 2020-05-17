435b72ac7ccebc1ea9e6e55c5d943ee3cb23c9bc
Gêdhean Alves <gedhean.alves@gocase.com.br>
2020-05-17 16:57:01 -0300

    refactoring with factory functions

--- src/components/__test__/ItemList.spec.js
+++ src/components/__test__/ItemList.spec.js
@@ -1,5 +1,5 @@
 import flushPromises from "flush-promises";
-import { mount, shallowMount, createLocalVue } from "@vue/test-utils";
+import { shallowMount, createLocalVue } from "@vue/test-utils";
 import Vuex from "vuex";
 
 import Item from "./../Item.vue";
@@ -10,28 +10,39 @@ jest.mock("../../api/api.js");
 const localVue = createLocalVue();
 localVue.use(Vuex);
 
+const defaultStoreConfig = {
+  getters: { displayNews: jest.fn() },
+  actions: { fetchListData: jest.fn(() => Promise.resolve([])) }
+};
+
+function createStore(overrides) {
+  return new Vuex.Store({ ...defaultStoreConfig, ...overrides });
+}
+
+function createWrapper(overrides) {
+  const defaultMountOptions = {
+    mocks: {
+      $bar: {
+        start: jest.fn(),
+        fail: jest.fn(),
+        finish: jest.fn()
+      }
+    },
+    localVue,
+    store: createStore(),
+    ...overrides
+  };
+
+  return shallowMount(ItemList, defaultMountOptions);
+}
+
 describe("ItemList.vue", () => {
-  let store, storeConfig, $bar;
-  beforeEach(() => {
-    storeConfig = {
-      getters: { displayNews: jest.fn() },
-      actions: { fetchListData: jest.fn(() => Promise.resolve([])) }
-    };
-    store = new Vuex.Store(storeConfig);
-
-    $bar = {
-      start: jest.fn(),
-      fail: jest.fn(),
-      finish: jest.fn()
-    };
-  });
-
   test("render an Item components for each item", async () => {
     expect.assertions(4);
     const items = [{ id: 1 }, { id: 2 }, { id: 3 }];
-    storeConfig.getters.displayNews.mockReturnValue(items);
+    defaultStoreConfig.getters.displayNews.mockReturnValue(items);
 
-    const wrapper = mount(ItemList, { mocks: { $bar }, localVue, store });
+    const wrapper = createWrapper();
     await flushPromises();
 
     const Items = wrapper.findAll(Item);
@@ -44,49 +55,54 @@ describe("ItemList.vue", () => {
   });
 
   test("Item children receive right prop.item", () => {
-    const wrapper = mount(ItemList, { mocks: { $bar }, localVue, store });
+    const wrapper = createWrapper();
+
+    const items = [{ id: 1 }, { id: 2 }, { id: 3 }];
+    defaultStoreConfig.getters.displayNews.mockReturnValue(items);
 
     const Items = wrapper.findAll(Item);
 
     Items.wrappers.forEach((item, idx) => {
-      expect(item.props().item).toBe(window.items[idx]);
+      expect(item.props().item).toBe(
+        wrapper.vm.$store.getters.displayNews[idx]
+      );
     });
   });
 
   // TODO: figure out why mount is calling beforeMount hook twice
   test("calls $bar.start on load", () => {
-    mount(ItemList, { mocks: { $bar }, localVue, store });
-
-    expect($bar.start).toHaveBeenCalled();
+    const wrapper = createWrapper();
+    expect(wrapper.vm.$bar.start).toHaveBeenCalled();
   });
 
   test("calls $bar.finish when load data successful", async () => {
     expect.assertions(1);
-
-    shallowMount(ItemList, { mocks: { $bar }, localVue, store });
+    const wrapper = createWrapper();
 
     await flushPromises();
 
-    expect($bar.finish).toHaveBeenCalled();
+    expect(wrapper.vm.$bar.finish).toHaveBeenCalled();
   });
 
   test("calls $bar.fail when load data unsuccessful", async () => {
     expect.assertions(1);
     // Ensue the fetch fails
-    storeConfig.actions.fetchListData.mockRejectedValueOnce();
-
-    shallowMount(ItemList, { mocks: { $bar }, localVue, store });
+    defaultStoreConfig.actions.fetchListData.mockRejectedValueOnce();
+    const wrapper = createWrapper();
 
     await flushPromises();
 
-    expect($bar.fail).toHaveBeenCalled();
+    expect(wrapper.vm.$bar.fail).toHaveBeenCalled();
   });
 
   test('dispatch fetchListData with "top"', async () => {
     expect.assertions(1);
+    const store = createStore();
 
     store.dispatch = jest.fn(() => Promise.resolve());
-    shallowMount(ItemList, { mocks: { $bar }, localVue, store });
+
+    createWrapper({ store });
+
     await flushPromises();
 
     expect(store.dispatch).toHaveBeenCalledWith("fetchListData", {
